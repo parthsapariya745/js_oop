895af3de71be8b58a413ae9c3359252573bdc6f5
parthsapariya745 <parthsapariyait7@gmail.com>
2025-07-30 19:50:34 +0530

    update

--- class_object/2.js
+++ class_object/2.js
@@ -1,16 +1,16 @@
 class city {
     constructor(name, population) {
-        this.name = name;
-        this.population = population;
+        this.name = name
+        this.population = population
     }
     
     cityDetails() {
-        console.log(`City name is ${this.name}`);
-        console.log(`Population is ${this.population}`);
+        console.log(`City name is ${this.name}`)
+        console.log(`Population is ${this.population}`)
     }
 }
 let city1 = new city("Rajkot", 2150330).cityDetails()
 
-console.log("------------------------------------");
+console.log("------------------------------------")
 
 let city2 = new city("Ahmedabad", 9062000).cityDetails()
