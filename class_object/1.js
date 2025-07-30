c2be55c88a19a857976c87312106ff9199c69f34
parthsapariya745 <parthsapariyait7@gmail.com>
2025-07-30 16:32:02 +0530

    first commit

---
+++ class_object/1.js
@@ -0,0 +1,10 @@
+let food = {
+    name: "Pizza",
+    price: 150,
+
+    display: function() {
+        console.log(`Food name is ${this.name}`);
+        console.log(`Price is ₹${this.price}`);
+    }
+}
+food.display();
---
+++ inheritance/script.js
@@ -0,0 +1,18 @@
+class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    details() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+class parth extends person {
+    walk() {
+        console.log(`${this.name} can walk.`);
+    }
+}
+let p1 = new parth("Parth", 19)
+p1.details()
+p1.walk();
